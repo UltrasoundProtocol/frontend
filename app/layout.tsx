d5c2a6f0fcd4d47a7aaa5734af47c0694ddbb1ee
Giulianomlodi <giulianomlodi@gmail.com>
2025-11-14 17:52:04 +0100

    replaced Opengraph preview image for the shared url

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/Preview.png",
-        width: 976,
-        height: 976,
+        width: 1200,
+        height: 630,
         alt: "ULTRASOUND - BTC GOLD Strategy",
       },
     ],
